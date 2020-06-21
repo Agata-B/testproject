74591b949f0a1d4c75f17801c9d03c97e7d17b30
Agata <agatabienkowska1@wp.pl>
2020-06-21 08:37:47 +0200

    Add Ex4_6

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,34 @@
 import homework1SDA.Ex1_3;
+import homework1SDA.Ex4_6;
 
 public class Main {
 
     public static void main(String[] args) {
 
+        System.out.println("Ex1_3 .1");
         Ex1_3 ex1_3 = new Ex1_3();
         ex1_3.multiplicationTable(3);
 
-        System.out.println();
 
+        System.out.println("Ex1_3 .2");
         System.out.println(ex1_3.itIsAPrimeNumber());
 
+        System.out.println("Ex1_3 .3");
         System.out.println(ex1_3.add());
+
+        System.out.println("Ex4_6 .4");
+        int[] tab = {5, 20, 63};
+        Ex4_6.invertedArray(tab);
+        System.out.println();
+
+        System.out.println("Ex4_6 .5");
+        int[] array = {10, 2, 150};
+        Ex4_6.minMaxValue(array);
+
+        System.out.println("Ex4_6 .6");
+        int [] table = {-2, 20, -15, 1, -4};
+        int [] minusTable = Ex4_6.counterMinusValue(table);
+        Ex4_6.arrayPrinter(minusTable);
     }
 }
 
---
+++ src/main/java/homework1SDA/Ex4_6.java
@@ -0,0 +1,75 @@
+package homework1SDA;
+
+import java.awt.print.Printable;
+
+public class Ex4_6 {
+
+    /*4. Dla zadanej tablicy 3 intów zamień miejscami pierwszy i trzeci element.
+Przykład: zadana tablica [12, 7, 19]
+                   wynik [19, 7, 12]  */
+
+    public static void invertedArray(int[] arrayOfValues) {
+        if (arrayOfValues.length >= 3) {
+            System.out.print("Podana tablica to :    ");
+            arrayPrinter(arrayOfValues);
+            int value = arrayOfValues[0];
+            arrayOfValues[0] = arrayOfValues[2];
+            arrayOfValues[2] = value;
+            System.out.println();
+            System.out.print("Odwrócona tablica to : ");
+            arrayPrinter(arrayOfValues);
+        } else {
+            System.out.println("Error");
+        }
+    }
+
+    public static void arrayPrinter(int[] array) {
+        for (int value : array) {
+            System.out.print("[" + value + "]");
+        }
+    }
+
+
+    /*5. Dla zadanej tablicy intów wyświetl najmniejszą oraz największą liczbę z tablicy*/
+
+    public static void minMaxValue(int[] table) {
+        int min = table[0];
+        int max = table[0];
+        for (int i = 0; i < table.length; i++) {
+            if (min > table[i]) {
+                min = table[i];
+            }
+            if (max < table[i]) {
+                max = table[i];
+            }
+        }
+        System.out.println("Wartość minimalna z tablicy to : " + min);
+        System.out.println("Wartość maksymalna z tablicy to : " + max);
+
+    }
+/*6. Dla zadanej tablicy intów policz ile jest w niej liczb ujemnych. Jeśli takie występują
+utwórz nową tablicę, do której przepisze tylko te ujemne liczby*/
+
+    public static int[] counterMinusValue(int[] tab) {
+        int counter = 0;
+        for (int num : tab) {
+            if (num < 0) counter++;
+        }
+        int[] minus = new int[counter];
+        int sum = 0;
+        for (int num : tab) {
+            if (num < 0) {
+                minus[sum] = num;
+                sum++;
+            }
+        }
+        System.out.println("Suma liczb ujemnych w tablicy: " + counter);
+        System.out.print("Tablica wartości ujemnych z wcześnie utworzonej tablicy: ");
+        arrayPrinter(minus);
+        return minus;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
